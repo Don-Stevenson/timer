1dacd3cd47b5d84a5f68d7dd4c62e8ee1998e012
Don-Stevenson <dsteven1@gmail.com>
2019-10-22 19:55:51 +0000

    updated code to include to conditional tests

--- timer1.js
+++ timer1.js
@@ -3,20 +3,24 @@ args = args.slice(2);
 let num = [];
 
 const alarm = function(time){
-  setTimeout(() => {
+  if (time === undefined){
+    return;
+  }
+setTimeout(() => {
     process.stdout.write('\x07');
     console.log('alarm rang', time);
   }, time*1000);
 }
 
 for (let letter of args){
-  letter = Number(letter);
+letter = Number(letter);
+  if(letter >= 1){
   num.push(letter); 
+  } 
 }
 // console.log(num);
 
 const ringAlarm = function (array, callback, stop){
-
   for(let item of array){
     callback(item);
     // console.log('!!!!', item);
